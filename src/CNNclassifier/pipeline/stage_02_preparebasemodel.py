169307d185f3c08aafcedffc0dcc67147b537475
megha-singh30 <megha0330@gmail.com>
2026-06-10 17:44:08 -0400

    DVC run. Added dvc.lock

--- src/CNNclassifier/pipeline/stage_02_preparebasemodel.py
+++ src/CNNclassifier/pipeline/stage_02_preparebasemodel.py
@@ -12,7 +12,7 @@ class PrepareBaseModelTrainingPipeline:
     def main(self):
         config = ConfigurationManager() 
         prepare_base_model_config = config.get_prepare_base_model_config()
-        prepare_base_model = PrepareBaseModel(config= prepare_base_model_config)
+        prepare_base_model = PrepareBaseModel(config = prepare_base_model_config)
         prepare_base_model.get_base_model()
         prepare_base_model.update_base_model()
 
@@ -21,7 +21,7 @@ if __name__=="__main__":
     try:
         logger.info(f"**************")
         logger.info(f">>>>>>>>stage {STAGE_NAME} started <<<<<<<<<<<<")
-        obj = PrepareBaseModel()
+        obj = PrepareBaseModelTrainingPipeline()
         obj.main()
         logger.info(f">>>>>>>>stage {STAGE_NAME} completed <<<<<<<<<<<<")
     except Exception as e:
